366fa9f1335bdcfd77fe1f9d6a3b65ddc06e69d7
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2024-06-21 10:33:10 +0300

    add chrome headless in our project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,8 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -13,10 +17,10 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
 import utilities.Helper;
 
-public class TestBase extends AbstractTestNGCucumberTests
+public class TestBase //extends AbstractTestNGCucumberTests  (The commented section is used with cucumber)
 {
 	public static WebDriver driver;
 	
@@ -28,11 +32,11 @@ public class TestBase extends AbstractTestNGCucumberTests
 	{
 		 System.out.println("Browser Name: " + browserName); // Add this line for debugging
 		if (browserName.equalsIgnoreCase("chrome")) {
-			//System.setProperty("webdrvier.chrome.driver", System.getProperty("user.dir"+"/drivers/chromedriver.exe"));
+			//System.setProperty("webdrvier.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
 			driver = new ChromeDriver();
 		}
 		else if(browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdrvier.gecko.driver", System.getProperty("user.dir"+"/drivers/geckodriver.exe"));
+			System.setProperty("webdrvier.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
 			driver = new FirefoxDriver();
 		}
 		else if(browserName.equalsIgnoreCase("edge")) {
@@ -42,6 +46,28 @@ public class TestBase extends AbstractTestNGCucumberTests
 		else if(browserName.equalsIgnoreCase("safari")) {
 			driver = new SafariDriver();
 		}
+		
+		// Headless browser testing
+		else if(browserName.equalsIgnoreCase("headless")) {
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setCapability("javascriptEnabled", true);
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+					System.getProperty("user.dir")+"/drivers/phantomjs.exe");
+			String[] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, phantomJsArgs);
+			driver = new PhantomJSDriver(caps);
+		}
+		
+		// Headless chrome
+		else if(browserName.equalsIgnoreCase("chrome-headless")) {
+			//System.setProperty("webdrvier.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--headless");
+			options.addArguments("--window-size=1920,1080");
+			driver = new ChromeDriver(options);
+			
+		}
+		
     	driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.navigate().to("https://demo.nopcommerce.com/");
