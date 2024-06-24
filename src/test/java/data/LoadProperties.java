0787b1609e1069e7dcd0863eb9e5ae164e05063f
Mohammad Alaa <mohammad.alaaeldin98@gmail.com>
2024-06-24 21:35:11 +0300

    Running Using Saucelabs

--- src/test/java/data/LoadProperties.java
+++ src/test/java/data/LoadProperties.java
@@ -10,6 +10,13 @@ public class LoadProperties
 	// Load the properties file from the folder
 	public static Properties userData = loadProperties(System.getProperty("user.dir") + "\\src\\main\\java\\properties\\userdata.properties");
 	
+	
+	// Load Sauce Labs Account Data
+	public static Properties sauceLabsData = loadProperties(System.getProperty("user.dir") + "\\src\\main\\java\\properties\\saucelabsUser.properties");
+	
+	
+	
+	
 	private static Properties loadProperties (String path)
 	{
 		Properties pro = new Properties();
